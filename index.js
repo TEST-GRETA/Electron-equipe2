5c6b287b0f0910eba6af820c1ba37fc14022b247
HemeraMedia <hemera.media@gmail.com>
2022-03-21 10:30:36 +0100

    test
    test

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const list = document.querySelector('ul');
+
+api.receive('todo:add', todo => {
+    const li = document.createElement('li');
+    const text = document.createTextNode(todo);
+
+    debugger;
+
+    li.appendChild(text);
+    list.appendChild(li);
+});
+
+api.receive('todo:clear', () => {
+    list.innerHTML = '';
+})
---
+++ main.js
@@ -0,0 +1,165 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+const isMac = process.platform === 'darwin'
+
+let mainWindow;
+let addWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 750,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  !isMac && mainWindow.on('closed', () => app.quit());
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+})
+
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit()
+})
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 600,
+    height: 700,
+    title: "Ajouter un nouvel utilisateur",
+    // frame: false,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
+ipcMain.on("todo:add", (event, todo) => {
+  mainWindow.webContents.send("todo:add", todo);
+  addWindow.close();
+});
+
+const menuTemplate = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close', accelerator: "Command+Q" } : { role: 'quit', accelerator: "Ctrl+Q" },
+      {
+        label: "Nouvel utilisateur", accelerator: "Ctrl+N",
+        click() { createAddWindow(); },
+      }
+      // {
+      //   label: "Réinitialise la liste", accelerator: "Ctrl+R",
+      //   click() {
+      //     mainWindow.webContents.send("todo:clear")
+      //   }
+      // }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
